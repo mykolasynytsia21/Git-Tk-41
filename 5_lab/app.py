ce7634974fdad8cc4dd9ce8683326c0653c66405
mykolasynytsia21 <msynytsia2019@itcollege.lviv.ua>
2022-10-29 14:47:10 +0200

    5_lab

---
+++ 5_lab/app.py
@@ -0,0 +1,16 @@
+class Figure:
+    FIGURES = ["квадрат", "прямокутник", "трикутник"]
+    def __init__(self, type, length) -> None:
+        assert length > 0, "Довжина має бути більшою за 0!"
+        assert type in self.FIGURES, "Дозволені фігури: квадрат, прямокутник, трикутник"
+        self.type = type
+        self.length = length
+
+    @property
+    def get_figure_type(self):
+        return self.type
+
+    @property
+    def get_figure_length(self):
+        return self.type # робимо помилку
+        
